01ce02d08b6f395e6e71efd9c82222f4d1a25b58
kaisa <kaisaohnae@gmail.com>
2025-10-21 15:09:11 +0900

    ..

--- src/app/main/main-visual.tsx
+++ src/app/main/main-visual.tsx
@@ -1,53 +1,27 @@
 'use client';
 
-import React, {ReactElement, useEffect, useRef} from 'react';
+import React, { ReactElement, useEffect, useRef } from 'react';
 import mainVisualList from '@/data/main-visual-list';
 import SwiperCore from 'swiper';
-import {Autoplay, Navigation, Pagination} from 'swiper/modules';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 
 SwiperCore.use([Navigation, Pagination, Autoplay]);
 
-export default function Page(): ReactElement {
+export default function MainVisual(): ReactElement {
   const ready = useRef(false);
-  const swiperContainerRef = useRef<HTMLElement | null>(null);  // 타입을 지정
+  const swiperContainerRef = useRef<HTMLElement | null>(null);
   const swiperInstance = useRef<SwiperCore | null>(null);
 
   useEffect(() => {
     if (!ready.current) {
       ready.current = true;
-      swiperInstance.current = new SwiperCore(swiperContainerRef.current as HTMLElement, {  // 타입 단언 추가
-        initialSlide: 1,
+      swiperInstance.current = new SwiperCore(swiperContainerRef.current as HTMLElement, {
         loop: true,
         slidesPerView: 1,
-        spaceBetween: 0,
-        centeredSlides: true,
-        effect: 'coverflow',
-        coverflowEffect: {
-          rotate: 0,
-          slideShadows: false,
-        },
-        autoplay: {
-          delay: 2500, // 자동 슬라이드 간의 지연 시간 (밀리초)
-          disableOnInteraction: true, // 사용자가 슬라이드를 조작할 때 자동 재생 중지
-        },
-        observer: true,
-        observeParents: true,
-        navigation: {
-          nextEl: '.swiper-next-1',
-          prevEl: '.swiper-prev-1',
-        },
-        breakpoints: {
-          500: {
-            slidesPerView: 1,
-          },
-        },
-        on: {
-          slideChange: () => {
-            // 슬라이드 변경 시 동작
-          },
-        },
+        autoplay: { delay: 5500, disableOnInteraction: false },
+        pagination: { el: '.swiper-pagination1', clickable: true },
+        navigation: { nextEl: '.swiper-next-1', prevEl: '.swiper-prev-1' },
       });
-
     }
   }, []);
 
@@ -55,19 +29,17 @@ export default function Page(): ReactElement {
     <div className="slide-wrap">
       <article ref={swiperContainerRef} className="preview-slide">
         <ul className="swiper-wrapper">
-          {
-            mainVisualList.map((o: any, idx: number) => (
-              <li className="swiper-slide" key={idx}><img src={o.url} alt=''/></li>
-            ))
-          }
+          {mainVisualList.map((o: any, idx: number) => (
+            <li className="swiper-slide" key={idx}>
+              <div className="visual-image">
+                <img src={o.url} alt="" />
+              </div>
+            </li>
+          ))}
         </ul>
-        <div className="swiper-pagination swiper-pagination1 swiper-pagination-bullets swiper-pagination-horizontal"></div>
-        <div className="swiper-button swiper-prev swiper-prev-1">
-          <i className="icon">&#xf104;</i>
-        </div>
-        <div className="swiper-button swiper-next swiper-next-1">
-          <i className="icon">&#xf105;</i>
-        </div>
+        <div className="swiper-pagination swiper-pagination1"></div>
+        <div className="swiper-button swiper-prev swiper-prev-1">&#10094;</div>
+        <div className="swiper-button swiper-next swiper-next-1">&#10095;</div>
       </article>
     </div>
   );
