640282fe03234dcb82bee04831c8f17755032bce
kaisa <kaisa@pumpkinev.co.kr>
2026-08-24 17:53:52 +0900

    ..

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: false,
   output: 'export',
   trailingSlash: true,
-  eslint: {
-    // ESLint 에러를 무시하고 빌드 진행
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
